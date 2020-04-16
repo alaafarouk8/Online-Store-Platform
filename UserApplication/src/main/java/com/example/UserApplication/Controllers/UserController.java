ccd02592f35015f762e70fa0f6f2df6ea0897a83
alaafar256 <58630587+alaafar256@users.noreply.github.com>
2020-04-16 18:24:52 +0200

    update files

--- UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
+++ UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,16 +40,14 @@ public class UserController {
 	     
 	    return "redirect:/";
 	}
-	@RequestMapping("/loginforusers")
-	public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
 
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
-        return "loginforusers";
-    }
-
+	@RequestMapping("/login")
+	public String login(Model model) {
+	    User user = new User();
+	    model.addAttribute("user",user);
+	     
+	    return "login";
+	}
+	
 	
 }
