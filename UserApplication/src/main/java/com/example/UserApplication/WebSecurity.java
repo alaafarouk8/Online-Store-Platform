e1964785e1463a6dcb88c2d3eb8e9e244eef13ef
Maria <mariaemil55@gmail.com>
2020-04-16 03:29:09 +0200

    working login

---
+++ UserApplication/src/main/java/com/example/UserApplication/WebSecurity.java
@@ -0,0 +1,52 @@
+package com.example.UserApplication;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import Models.User;
+import Services.UserService;
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private DataSource  datasource ;
+	@Autowired
+	public void Authentication(AuthenticationManagerBuilder authBuilder) throws Exception
+	{
+		authBuilder.jdbcAuthentication()
+		.dataSource(datasource)
+		.passwordEncoder(new BCryptPasswordEncoder())
+		.usersByUsernameQuery("select name, password, enabled from user_login where name=?") 
+		.authoritiesByUsernameQuery("select name,gender from user_login where name=?"); 
+	}  
+	
+  @Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.permitAll()
+		.and()
+		.logout()
+		.permitAll() ; 
+		
+	} 
+	 
+
+}
