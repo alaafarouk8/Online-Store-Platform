7e78676f5f675438cc1d19c9119aea7dbafe2859
Maria <mariaemil55@gmail.com>
2020-04-17 16:18:29 +0200

    Revert "A few minor changes"
    This reverts commit f439cf7f3351f63cf959c0fe3ed08fc4bef5d6dd.

--- UserApplication/src/main/java/com/example/UserApplication/WebSecurity.java
+++ UserApplication/src/main/java/com/example/UserApplication/WebSecurity.java
@@ -1,13 +1,19 @@
 package com.example.UserApplication;
 
 import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import Models.User;
 import Services.UserService;
