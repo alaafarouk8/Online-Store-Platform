c7846f0387facde33fdcc722654d820c32e44553
Maria <mariaemil55@gmail.com>
2020-04-17 16:18:59 +0200

    Revert "A few minor changes"
    This reverts commit ea72d317bf53ea2a9ed20541a637912b809ce63f.

--- UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
+++ UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.example.UserApplication.Controllers;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< Updated upstream:UserApplication/src/main/java/com/example/Controllers/UserController.java
 
 import com.example.UserApplication.Models.User;
 import com.example.UserApplication.Services.UserService;
@@ -14,12 +14,6 @@ import com.example.UserApplication.Services.UserService;
 
 
 
-=======
-import com.example.UserApplication.Models.User;
-import com.example.UserApplication.Services.UserService;
-
-
->>>>>>> Stashed changes:UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
 @Controller
 public class UserController {
 	@Autowired
@@ -35,13 +29,13 @@ public class UserController {
 	@RequestMapping("/new")
 	public String adduser(Model model) {
 	    User user = new User();
-	    model.addAttribute("user",user);     
+	    model.addAttribute("user",user);
+	     
 	    return "NewUser";
 	}
 	
-	@RequestMapping("/save")
+	@RequestMapping(value="/save")
 	public String saveuser(@ModelAttribute("user") User user) {
-<<<<<<< Updated upstream:UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
 	      service.adduser(user);
 	     if (user.getId()==1) {
 	      return "redirect:/";
@@ -58,28 +52,6 @@ public class UserController {
 	    model.addAttribute("user",user);
 	     
 	    return "login";
-=======
-	    service.adduser(user);
-<<<<<<< Updated upstream:UserApplication/src/main/java/com/example/Controllers/UserController.java
-	     
-	    return "redirect:/";
-=======
-	    if(user.getId()==1) {
-	    return "redirect:/";
-	    }
-	    else 
-	    {
-	    	return"error";
-	    }
-	}
-
-	@RequestMapping("/login")
-	public String login(Model model) {
-	    User user = new User();
-	    model.addAttribute("user",user);     
-	    return "login";
->>>>>>> Stashed changes:UserApplication/src/main/java/com/example/UserApplication/Controllers/UserController.java
->>>>>>> Stashed changes:UserApplication/src/main/java/com/example/Controllers/UserController.java
 	}
 	
 	
--- UserApplication/src/main/java/com/example/UserApplication/Services/UserService.java
+++ UserApplication/src/main/java/com/example/UserApplication/Services/UserService.java
@@ -1,21 +1,12 @@
 package com.example.UserApplication.Services;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< Updated upstream:UserApplication/src/main/java/Services/UserService.java
 
-<<<<<<< Updated upstream:UserApplication/src/main/java/com/example/UserApplication/Services/UserService.java
 import com.example.UserApplication.DA.UserRepository;
 import com.example.UserApplication.Models.User;
-=======
-import DA.UserInterface;
-import Models.User;
-=======
-import com.example.UserApplication.DA.UserRepository;
-import com.example.UserApplication.Models.User;
->>>>>>> Stashed changes:UserApplication/src/main/java/com/example/UserApplication/Services/UserService.java
->>>>>>> Stashed changes:UserApplication/src/main/java/Services/UserService.java
 
 @Service
 public class UserService {
